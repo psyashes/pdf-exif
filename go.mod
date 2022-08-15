7d66b481c0be8a0ecf5be3ef90c5f2a3f3a3623d
psyashes <43512814+psyashes@users.noreply.github.com>
2022-08-15 18:32:59 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module exif-pdf
+
+go 1.19
+
+require github.com/barasher/go-exiftool v1.8.0
+
+require github.com/h2non/bimg v1.1.9
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+  "os"
+	exiftool "github.com/barasher/go-exiftool"
+  "github.com/h2non/bimg"
+)
+
+func main() {
+  pdf := "./sample.pdf"
+  buffer, err := bimg.Read(pdf)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  jpeg, err := bimg.NewImage(buffer).Convert(bimg.JPEG)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  if bimg.NewImage(jpeg).Type() == "jpeg" {
+    fmt.Println("JPEG")
+  }
+
+  file, err := os.Create("./converted.jpeg")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer file.Close()
+
+  _, err = file.Write(jpeg)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  // Get Exif
+  jpeg_path := "./converted.jpeg"
+  getExif(jpeg_path)
+
+  // Remove images
+  os.Remove(jpeg_path)
+}
+
+func getExif(file string) {
+  et, err := exiftool.NewExiftool()
+  if err != nil {
+    fmt.Println("Error when initializing Exiftool:", err)
+  }
+  defer et.Close()
+
+  fileInfos := et.ExtractMetadata(file)
+
+  for _, fileInfo := range fileInfos {
+    if fileInfo.Err != nil {
+      fmt.Printf("Error concerning %v: %v\n", fileInfo.File, fileInfo.Err)
+      continue
+    }
+
+    for k, v := range fileInfo.Fields {
+      fmt.Printf("[%v] %v\n", k, v)
+    }
+  }
+}
